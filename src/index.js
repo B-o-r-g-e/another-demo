57e36ce5a8298ddcf68c2332480d212122ce81fc
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-02-01 05:12:24 +0100

    .

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const content = document.getElementById('content')
+
+function addContentContent() {
+    const nav = document.createElement('nav')
+
+    const navContent = document.createElement('div')
+    navContent.className = 'nav-content'
+
+    const logo = document.createElement('h2')
+    logo.textContent = `logo`
+
+}
+
+addContentContent()
